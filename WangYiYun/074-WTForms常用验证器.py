029810a2d07065ffad3c57b7ac06036d6ea3c69a
Gino-Y <ginoyan250@gmail.com>
2021-09-20 20:08:27 +0800

    add

--- WangYiYun/074-WTForms常用验证器.py
+++ WangYiYun/074-WTForms常用验证器.py
@@ -1,4 +1,16 @@
 from flask import Flask, request, render_template
+from wtforms import Form, StringField
+from wtforms.validators import Length, EqualTo
+
+
+class RegistForm(Form):
+    username = StringField(validators=[Length(min=3, max=10,
+                                              message='用户名长度必须在3到10位之间')])
+    password = StringField(validators=[Length(min=3, max=10,
+                                              message='用户名长度必须在3到10位之间')])
+    password_repeat = StringField(validators=[Length(min=3, max=10,
+                                              message='两次密码输入不一致'), EqualTo('password')])
+
 
 app = Flask(__name__)
 
@@ -13,7 +25,13 @@ def regist():
     if request.method == 'get':
         return render_template(r'WangYiYun/templates/regist.html')
     else:
-        pass
+        form = RegistForm(request.form)
+        if form.validate():
+            return 'success'
+        else:
+            print(form.errors)
+            return 'fail'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
