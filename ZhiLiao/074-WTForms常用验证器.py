87100704bebed1d3c8640f786db55724c0f77475
Gino-Y <ginoyan250@gmail.com>
2021-09-20 18:11:38 +0800

    add

--- ZhiLiao/074-WTForms常用验证器.py
+++ ZhiLiao/074-WTForms常用验证器.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def hello_world():
 
 @app.route('/regist/', methods=['GET'])
 def regist():
-    if regist.method == 'get':
+    if request.method == 'get':
         pass
     else:
         pass
