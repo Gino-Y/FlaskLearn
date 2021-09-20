dca33b9cad0105632e6c038791f82e26a43427a2
Gino-Y <ginoyan250@gmail.com>
2021-09-20 15:48:00 +0800

    add

--- Learn-02/p08_session_login_logout.py
+++ Learn-02/p08_session_login_logout.py
@@ -1,17 +1,19 @@
-from flask import Flask,request,make_response,session,redirect,jsonify
+from flask import Flask, request, make_response, session, redirect, jsonify
+
 app = Flask(__name__)
 
-#pip install flask-cors
-from flask_cors import *  # 导入模块
-CORS(app, supports_credentials=True)  # 设置跨域
+# #pip install flask-cors
+# from flask_cors import *  # 导入模块
+# CORS(app, supports_credentials=True)  # 设置跨域
 
-app.config["SECRET_KEY"]="123"
+app.config["SECRET_KEY"] = "123"
 users = {
-    "zhangsan":"1234",
-    "lisi":"1234"
+    "zhangsan": "1234",
+    "lisi": "1234"
 }
 
-@app.route("/login",methods=["POST"])
+
+@app.route("/login", methods=["POST"])
 def login():
     username = request.form.get("username")
     password = request.form.get("password")
@@ -22,22 +24,25 @@ def login():
     else:
         return '{"code":1,"msg":"用户名或密码错误"}'
 
-@app.route("/logout",methods=["GET"])
+
+@app.route("/logout", methods=["GET"])
 def logout():
     session["username"] = None
     return '{"code":0,"msg":"success"}', \
            200, {"content-type": "application/json"}
 
+
 def isLogin():
     return "username" in session and session["username"] is not None
 
-@app.route("/getLoginUser",methods=["GET"])
+
+@app.route("/getLoginUser", methods=["GET"])
 def getLoginUser():
     if not isLogin():
         return '{"code":3,"msg":"未登陆"}'
-    r = {"code":0,"msg":"success","data":session["username"]}
+    r = {"code": 0, "msg": "success", "data": session["username"]}
     print(jsonify(**r))
-    return jsonify(**r) # jsonify(code=0,msg="success",..)
+    return jsonify(**r)  # jsonify(code=0,msg="success",..)
 
 
 if __name__ == '__main__':
