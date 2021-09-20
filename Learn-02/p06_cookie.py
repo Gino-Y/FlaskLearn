65b01782b6074d8a1ce726ddb71090592c5cc989
Gino-Y <ginoyan250@gmail.com>
2021-09-20 17:44:10 +0800

    add

--- Learn-02/p06_cookie.py
+++ Learn-02/p06_cookie.py
@@ -1,27 +1,26 @@
-
-from flask import Flask,request,make_response
+from flask import Flask, request, make_response
 import tokenUtil
+
 app = Flask(__name__)
 
 # {"parkId":"1","status":"空闲"}
 #
 parkDict = {
-    "1":"空闲",
-    "2":"空闲",
-    "3":"空闲"
+    "1": "空闲",
+    "2": "空闲",
+    "3": "空闲"
 }
 userDict = {
-    "zs":"123456",
-    "ls":"123456"
+    "zs": "123456",
+    "ls": "123456"
 }
-
 
 # session
 
-sessionStore = {"001":session1 }
+sessionStore = {"001": session1}
 
 
-@app.route("/login",methods=["GET","POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     resp = make_response("LOGIN SUCCESS")
     resp.set_cookie("sessionId", "001", path="/")
@@ -29,22 +28,25 @@ def login():
     session["001"]["username"] = request.args.get("username")
     return resp
 
-@app.route("/main",methods=["GET","POST"])
+
+@app.route("/main", methods=["GET", "POST"])
 def amin():
     if "sessionId" not in request.cookies:
         return "NOT LOGIN"
     else:
         sessionId = request.cookies.get("sessionId")
-        return "WELCOME,"+session[sessionId]["username"]
+        return "WELCOME," + session[sessionId]["username"]
 
-@app.route("/getCookie/aaa/bbb/ccc",methods=["GET","POST"])
+
+@app.route("/getCookie/aaa/bbb/ccc", methods=["GET", "POST"])
 def getToken():
     resp = make_response("SUCCESS")
-    resp.set_cookie("sessionId","001",path="/getInfo/")
-    resp.set_cookie("cookie004","bbbb",max_age=10)
+    resp.set_cookie("sessionId", "001", path="/getInfo/")
+    resp.set_cookie("cookie004", "bbbb", max_age=10)
     return resp
 
-@app.route("/getInfo/aaa/b",methods=["GET"])
+
+@app.route("/getInfo/aaa/b", methods=["GET"])
 def getInfo():
     return request.cookies.get("cookie005")
 
